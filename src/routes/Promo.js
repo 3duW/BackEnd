224a26fc1be6699cf5571b218795de3c76ae5bfc
3duW <71583178@certus.edu.pe>
2023-11-23 01:41:48 -0500

    SE ARREGLO EL PROBLEMA DE CONEXCION AL PUERTO

--- src/routes/Promo.js
+++ src/routes/Promo.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const PromoSchema = require("../models/Promociones");
+const PromoSchema = require("../models/Prom");
 
 /**
  * @swagger
