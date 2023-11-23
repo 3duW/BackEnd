bc0ef786043116334b12afb14c9e133137332ccd
3duW <71583178@certus.edu.pe>
2023-11-23 17:52:29 -0500

    SE MODIFICARON ALGUNAS COSAS

--- src/models/coment.js
+++ src/models/coment.js
@@ -1,25 +1,32 @@
+
+// Importing mongoose module
 const mongoose = require("mongoose")
 
+// Defining a schema for the comment model
 const comentmodel = mongoose.Schema({
-    cliente:{
+    // Client name
+    cliente: {
         type: String,
         require: true
     },
-    fecha:{
+    // Date of the comment
+    fecha: {
         type: String,
         require: true
     },
-    calificacion:{
+    // Rating given by the client
+    calificacion: {
         type: String,
         require: true
     },
-    comentario:{
+    // Comment text
+    comentario: {
         type: String,
         require: true,
     }
-    
 });
 
-
- 
+// Creating a model named "comentario" using the defined schema
 module.exports = mongoose.model("comentario", comentmodel);
+//
+//In this code, we define a schema for a comment model. Each comment has a client name, a date, a rating, and a comment text. The schema is then used to create a model named "comentario". This model can be used to interact with the MongoDB database..</s>
