e47e44515458551159829b081e3e31eb7228acff
3duW <71583178@certus.edu.pe>
2023-12-07 21:56:31 -0500

    BACK FUNCIONA

--- src/index.js
+++ src/index.js
@@ -41,8 +41,6 @@ const swaggerConf = {
     },
     apis: [ ` ${path.join(__dirname, "./routes/*.js")} ` ]
 }
-
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
